8561890ea89cfc2b985189baeff461d7a9c2fe2e
Muhammad Arshman <itxarshman@gmail.com>
2025-11-02 23:08:59 +0500

    Update core.js

--- modules/core.js
+++ modules/core.js
@@ -5,6 +5,10 @@ import { exec } from 'child_process';
 import helpers from '../utils/helpers.js';
 import logger from '../core/logger.js';
 import { spawn } from 'child_process';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 class CoreModule {
     constructor(bot) {
